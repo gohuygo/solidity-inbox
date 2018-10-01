39ce62a37fcfa56ffffbdafd88046999ef40425f
Huy Nguyen <huy@gohuygo.com>
2018-10-01 16:31:23 -0700

    Installed .env to hide mnemonic phrase and api key. Setting up deploy script

---
+++ deploy.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC_PHRASE,
+  process.env.INFURA_API_KEY
+);
+
+const web3 = new Web3(provider);
