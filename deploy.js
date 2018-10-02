e1ade8d1e11f1137e7d0e9e41c2ddbdb0a448c99
Huy Nguyen <huy@gohuygo.com>
2018-10-01 17:37:33 -0700

    Deploy script done.

--- deploy.js
+++ deploy.js
@@ -11,3 +11,22 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+
+
+
+
+
+// Need function for async syntax
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from: ", accounts[0])
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+                            .deploy({ data: bytecode, arguments: ["Hello world"] })
+                            .send({ from: accounts[0], gas: '1000000' })
+
+  console.log("Contract deployed to: ", result.options.address);
+};
+deploy();
+
